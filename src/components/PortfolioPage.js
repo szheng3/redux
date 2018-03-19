e1027721e4482f76a4c0c1e39ba15320e28c1d8d
Sept.Griffith <szheng3@uci.edu>
2018-03-18 21:31:34 -0700

    second

--- src/components/PortfolioPage.js
+++ src/components/PortfolioPage.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
-const PortfolioPage = () => (
-  <div>
-    <h1>My Work</h1>
-    <p>Checkout the stuff I've done:</p>
-    <Link to="/portfolio/1">Item One</Link>
-    <Link to="/portfolio/2">Item Two</Link>
-  </div>
+
+const PortfolioPage = (props) => (
+    <div>
+        <h1>My Work</h1>
+        <p>Checkout the stuff I've done:</p>
+        <button onClick={(e) => {
+            console.log('sss');
+            props.history.push('/');
+        }}>test
+        </button>
+        <Link to="/portfolio/1">Item One</Link>
+        <Link to="/portfolio/2">Item Two</Link>
+    </div>
 );
+
 
 export default PortfolioPage;
